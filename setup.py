1732011b7bdc4d521c5506ccb3933bdf627f3dc1
OlaKenji <ola.kenji@gmail.com>
2025-12-05 10:59:28 +0100

    bump to 1.01

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="arpest",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     python_requires=">=3.7.13",
     install_requires=[
